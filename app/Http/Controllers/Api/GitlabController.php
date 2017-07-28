17efe9200ec4c498d4ceac340638f9ed6b3ad71a
Wannes Gennar <wannes.gennar@gmail.com>
2017-07-28 17:10:31 +0200

    add Gitlab route

---
+++ app/Http/Controllers/Api/GitlabController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+final class GitlabController extends Controller
+{
+    public function index()
+    {
+        return response();
+    }
+}
