830ff25061ee3cb6777510e6ac57d39df81fd37d
Wannes Gennar <wannes.gennar@gmail.com>
2017-07-28 17:01:31 +0200

    setup user seeder

---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+
+class UserTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $admin = (new User)->create([
+            'name'     => 'Administrator',
+            'email'    => 'admin@notifier.info',
+            'password' => bcrypt('secret'),
+        ]);
+    }
+}
